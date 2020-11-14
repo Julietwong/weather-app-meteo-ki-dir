fd4e8edcce745c032b6ccc966d7f4738e9c84e8c
Juliet Wong Min <julietwongmin@gmail.com>
2020-11-14 21:34:04 +1100

    Added text-transform capitalised to weather description

--- src/index.js
+++ src/index.js
@@ -1,76 +1,8 @@
-//Updating Time
-let currentTime=new Date();
-
-function updateDate(time){
-  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  let day = days[currentTime.getDay()];
-  let date = ('0' + currentTime.getDate()).slice(-2);
-  let months =["January","February","March","April","May","June","July","August","Septemver","October","November","December"];
-  let month = months[currentTime.getMonth()];
-  let hour= ('0' + currentTime.getHours()).slice(-2);
-  let minutes=currentTime.getMinutes();
-  let formattedDate=`${day}, ${date} ${month}, ${hour}:${minutes}`;
-  let currentDate=document.querySelector("#current-date-time");
-  currentDate.innerHTML = formattedDate;   
-}
-
-function updateForecastDay(time){
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let day1 = days[currentTime.getDay()];
-  let day2;
-  let day3;
-  let day4;
-  let day5;
-  let day6;
-
-  if((1+(currentTime.getDay()))>6){
-    day2 = days[1+(currentTime.getDay())-7];
-  }
-  else{
-    day2 = days[1+(currentTime.getDay())];
-  }
-  if((2+(currentTime.getDay()))>6){
-    day3 = days[2+(currentTime.getDay())-7];
-  }
-  else{
-    day3 = days[2+(currentTime.getDay())];
-  }
-  if((3+(currentTime.getDay()))>6){
-    day4 = days[3+(currentTime.getDay())-7];
-  }
-  else{
-    day4= days[3+(currentTime.getDay())];
-  }
-  if((4+(currentTime.getDay()))>6){
-    day5 = days[4+(currentTime.getDay())-7];
-  }
-  else{
-    day5 = days[4+(currentTime.getDay())];
-  }
-  
-  if((5+(currentTime.getDay()))>6){
-    day6 = days[5+(currentTime.getDay())-7];
-  }
-  else{
-    day6 = days[5+(currentTime.getDay())];
-  }
-   
-  let forecastDay1 = document.querySelector("#day-1");
-  forecastDay1.innerHTML = day1;
-  let forecastDay2 = document.querySelector("#day-2");
-  forecastDay2.innerHTML = day2;
-  let forecastDay3 = document.querySelector("#day-3");
-  forecastDay3.innerHTML = day3;
-  let forecastDay4 = document.querySelector("#day-4");
-  forecastDay4.innerHTML = day4;
-  let forecastDay5 = document.querySelector("#day-5");
-  forecastDay5.innerHTML = day5;
-  let forecastDay6 = document.querySelector("#day-6");
-  forecastDay6.innerHTML = day6;
-}
-
-updateDate (currentTime);
-updateForecastDay(currentTime);
+//Global variables
+let apiKey = "43b685724e0c77779a4487b322bb66db";
+let apiWeatherUrl = "api.openweathermap.org/data/2.5/weather";
+let apiCurrentLocationUrl = "api.openweathermap.org/data/2.5/find?"
+let units = "metric";
 
 //Update location
 function searchCity(event){
@@ -92,29 +24,7 @@ function searchCity(event){
 let form = document.querySelector("form");
 form.addEventListener("submit", searchCity);
 
-//Convert temperature
-function convertToCelcius(event){
-  event.preventDefault();
-  currentTemperature.innerHTML = 19;
-}
-function convertToFarenheit(event){
-  event.preventDefault();
-  currentTemperature.innerHTML= 66;
-}
-let currentTemperature = document.querySelector("#current-temperature");
-//console.log(currentTemperature);
-let celciusButton = document.querySelector("#unit-celcius");
-celciusButton.addEventListener("click", convertToCelcius);
-let farenheitButton = document.querySelector("#unit-farenheit");
-//console.log(farenheitButton);
-farenheitButton.addEventListener("click", convertToFarenheit);
-
-//Location
-let apiKey = "43b685724e0c77779a4487b322bb66db";
-let apiWeatherUrl = "api.openweathermap.org/data/2.5/weather";
-let apiCurrentLocationUrl = "api.openweathermap.org/data/2.5/find?"
-let units = "metric";
-
+//Update City
 function showSearchedCityWeather(response){
   console.log(response.data);
   let currentTemp = document.querySelector("#current-temperature");
@@ -133,7 +43,7 @@ function showSearchedCityWeather(response){
 
   let weatherDescription = document.querySelector("#current-weather-description");
   let weatherDescriptionValue=response.data.weather[0].description;   
-  weatherDescription.innerHTML = `${weatherDescriptionValue.charAt(0).toUpperCase()+weatherDescriptionValue.slice(1)}`;
+  weatherDescription.innerHTML = `${weatherDescriptionValue}`;
   }
 
   function showCurrentLocationWeather(response){
@@ -156,7 +66,7 @@ function showSearchedCityWeather(response){
 
   let weatherDescription = document.querySelector("#current-weather-description");
   let weatherDescriptionValue=response.data.list[0].weather[0].description;   
-  weatherDescription.innerHTML = `${weatherDescriptionValue.charAt(0).toUpperCase()+weatherDescriptionValue.slice(1)}`;
+  weatherDescription.innerHTML = `${weatherDescriptionValue}`;
     }
     else{
     console.log(response.data.main);
